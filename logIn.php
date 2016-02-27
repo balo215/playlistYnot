2de98f389525d7f2b28e6b0ade4ba17ed1fd88f5
Blas Gonzalez <balo215@hotmail.com>
2016-02-27 15:04:50 -0600

    bootstrap added, new style on some views

---
+++ logIn.php
@@ -0,0 +1,47 @@
+<?php
+    include_once "header.html";
+?>
+    <body>
+        <div id="container">
+            <header>
+                <h1>Wanna Make a playlist? Y not!?</h1>
+            </header>
+            <aside>
+                <div class="genre">
+                    <h3> List tittle </h3>
+                    <ul>
+                        <li><a href="#"> electroMusic </a> </li>
+                        <li><a href="#"> ejemplo </a> </li> 
+                        <li><a href="#"> tutos </a> </li>
+                    </ul>
+                </div>
+                <div class="genre">
+                    <h3> List tittle2 </h3>
+                    <ul>
+                        <li><a href= "#"> deadmau5 </a> </li>
+                        <li><a href="#"> adam k </a> </li> 
+                        <li><a href="#"> bt </a></li>
+                    </ul>
+                </div>
+            </aside>
+            <section>
+                <div class="signUpSection" id="login">
+                    <h3> Log in</h3>
+                    <p>    
+                        <input id="inputEmail" type="email" placeholder="User" class="form-control">
+                    </p>
+                    <p>
+                  
+                 
+                    <p>
+                        <input id="inputPwd" type="password" placeholder="password" class="form-control">
+                    </p>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </div>
+            </section>
+            <footer id="footer">
+                <span> aqui va el footer </span>
+            </footer>
+        </div>   
+    </body>
+</html>
