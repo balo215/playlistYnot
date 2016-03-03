7c78d8f9322930bfe4bbc7cebf0c94a48755781b
Blas Gonzalez <balo215@hotmail.com>
2016-03-02 22:59:18 -0600

    added header php, footer style

--- logIn.php
+++ logIn.php
@@ -6,39 +6,44 @@
             <header>
                 <h1>Wanna Make a playlist? Y not!?</h1>
             </header>
-            <aside>
-                <div class="genre">
-                    <h3> List tittle </h3>
-                    <ul>
-                        <li><a href="#"> electroMusic </a> </li>
-                        <li><a href="#"> ejemplo </a> </li> 
-                        <li><a href="#"> tutos </a> </li>
-                    </ul>
-                </div>
-                <div class="genre">
-                    <h3> List tittle2 </h3>
-                    <ul>
-                        <li><a href= "#"> deadmau5 </a> </li>
-                        <li><a href="#"> adam k </a> </li> 
-                        <li><a href="#"> bt </a></li>
-                    </ul>
-                </div>
-            </aside>
-            <section>
-                <div class="signUpSection" id="login">
-                    <h3> Log in</h3>
-                    <p>    
-                        <input id="inputEmail" type="email" placeholder="User" class="form-control">
-                    </p>
-                    <p>
-                  
-                 
-                    <p>
-                        <input id="inputPwd" type="password" placeholder="password" class="form-control">
-                    </p>
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                </div>
-            </section>
+            <div class="row">
+                <aside class="col-md-2 col-lg-2">
+                    <div class="panel panel-primary">
+                         <div class= "panel-heading">
+                            <h4 class="panel-title"> List tittle </h4>
+                         </div>
+                         <ul>
+                            <li><a href="#"> electroMusic </a> </li>
+                            <li><a href="#"> ejemplo </a> </li>
+                            <li><a href="#"> tutos </a> </li>
+                        </ul>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h4 class="panel-title"> List tittle2 </h4>
+                        </div>
+                        <ul>
+                            <li><a href= "#"> deadmau5 </a> </li>
+                            <li><a href="#"> adam k </a> </li>
+                            <li><a href="#"> bt </a></li>
+                        </ul>
+                    </div>
+                </aside>
+    
+                <section class="col-md-5 col-lg-5" id="about">
+                    <div class="signUpSection" id="login">
+                        <h3> Log in</h3>
+                        <p>    
+                            <input id="inputEmail" type="email" placeholder="User" class="form-control">
+                        </p>
+                        <p>
+                        <p>
+                            <input id="inputPwd" type="password" placeholder="password" class="form-control">
+                        </p>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                    </div>
+                </section>
+            </div>
             <footer id="footer">
                 <span> aqui va el footer </span>
             </footer>
